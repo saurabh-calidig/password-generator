fc0e9f9ca733139169c4c84ba046293a4a2a9c5c
Saurabh Negi <saurabh.negi@calidig.com>
2021-04-06 18:54:19 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+
+  length = 0;
+  password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  handleLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  handleLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  handleNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  handleSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  generatePassword() {
+    
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*-+_~';
+    let validChars = '';
+    
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
 }
